584956e9e29057c5971616d6b3c85fb9b9e0abcd
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-09 11:07:31 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 
 /**
- *main - print all combinations of three different digits
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int one;
-	int ten;
+	int tens;
+	int ones;
+	int t;
+	int o;
 
-		for (ten = '0'; ten <= '9'; ten++) /*tens=100s+1*/
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
-				putchar(ten);
-				putchar(one);
-				if (ten != '8' || one != '9')
+				for (o = ones + 1; o <= '9'; o++)
 				{
-					putchar(',');
+					putchar(tens);
+					putchar(ones);
 					putchar(' ');
+					putchar(t);
+					putchar(o);
+					if (!((tens == '9' && ones == '8') &&
+								(t == '9' && o == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				o = '0';
 			}
 		}
-	putchar('\n');
-
-	return (0);
+	}
+	putchar('\n')
+		return (0);
 }
