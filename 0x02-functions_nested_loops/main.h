06ecbf1c93d31a4d29cb3083067f1b235317fa6f
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-14 11:23:07 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,3 @@
-#include "main.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-
-{
-        print_alphabet_x10();
-        return (0);
-}
-
-
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-
 #ifndef MAIN_H
 #define MAIN_H
 
