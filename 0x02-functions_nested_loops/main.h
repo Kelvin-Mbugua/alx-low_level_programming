903f6d39f833f9bef83da83f6bdb64d077bcbfd1
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-14 11:21:01 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,27 @@
  * Return: Always 0.
  */
 int main(void)
+
+{
+        print_alphabet_x10();
+        return (0);
+}
+
+
 {
 	print_alphabet_x10();
 	return (0);
 }
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+
+
+
