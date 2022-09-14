12798e34bcf757cecb535d50550d113ca2cbcc6d
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-14 11:35:01 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,8 +7,14 @@
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+    int c;
+
+    r = _islower('H');
+    putchar(r + '0');
+    r = _islower('o');
+    putchar(r + '0');
+    r = _islower(108);
+    putchar(r + '0');
+    putchar('\n');
+    return (0);
 }
