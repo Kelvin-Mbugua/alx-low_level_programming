0df849f197672e1202218183c58ebec104475d60
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-14 12:32:58 -0700

    isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,8 +6,8 @@
 void positive_or_negative(int i);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
+void _islower(int c);
+void _isalpha(int c);
 
 #endif /* MAIN_H */
 
