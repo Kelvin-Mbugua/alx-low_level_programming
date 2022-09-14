23aeed36954d4753301bdb301842c988a184e35a
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-14 11:51:13 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,11 +7,15 @@
  * Return: 0 or 1
  */
 
-
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+	putchar('\n');
 }
