2d028636a859ffe722db0dc54da40c616dc8e840
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-14 11:06:49 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
