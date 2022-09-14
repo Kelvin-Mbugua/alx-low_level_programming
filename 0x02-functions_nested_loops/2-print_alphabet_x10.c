fb5e7d0b8a8b7f649d1688aeb3a616f4197374fe
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-14 11:03:33 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ *main - print lowercase a-z
+ *Return: Always 0 (Success)
  */
 
-int manin(void)
+int main(void)
 {
-	int count = 0;
-	char letter;
+	char alpha = 'a';
 
-	while (count++ <= 9)
+	while (alpha <= 'z')
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			putchar(letter);
-		putchar('\n');
+		putchar(alpha);
+		alpha++;
 	}
+	putchar('\n');
+
+	return (0);
 }
