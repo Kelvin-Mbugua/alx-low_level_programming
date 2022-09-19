5eefe4148ea226c5e9e9c4e91d812b2982e01752
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-09-19 09:51:48 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
+
 /**
  * main - generates keygen.
  * Return: 0 Always.
  */
-int main(void)
-{
-	int r = 0, c = 0;
-	time_t t;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, l, m, n;
+
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
 			break;
-		c = c + r;
-		printf("%c", r);
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-	printf("%c\n", (2772 - c));
-	return (0);
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
