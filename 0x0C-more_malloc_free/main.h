7679908df39bba4c1051d38cb875c0c8385eeabc
“Kelvin-Mbugua” <kmbugua.m@gmail.com>
2022-10-06 14:12:18 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
 int _putchar(char c)
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
